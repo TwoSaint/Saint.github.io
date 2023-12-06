b9f93572338904f2524afd7fd23f10b340d8baff
TwoSaint <67126354+TwoSaint@users.noreply.github.com>
2023-12-06 10:37:15 -0500

    Updates and fun

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 // Function to create and animate a bouncing ball
+// Function to create and animate a bouncing ball
 function createBall() {
     // Create a new ball element
+    const ballContainer = document.getElementById('ballContainer');
     const ball = document.createElement('div');
     ball.className = 'ball';
-    document.body.appendChild(ball);
+    ballContainer.appendChild(ball);
 
     // Set initial position randomly within the window
     let posX = Math.random() * window.innerWidth;
     let posY = Math.random() * window.innerHeight;
     ball.style.left = `${posX}px`;
     ball.style.top = `${posY}px`;
+
+    // Set a random color for the ball
+    ball.style.backgroundColor = getRandomColor();
 
     // Set initial velocity with random speed and direction
     let velX = (Math.random() - 0.5) * 10;
@@ -39,4 +44,14 @@ function createBall() {
 
     // Start the animation loop
     update();
-}
+  }
+
+  // Function to generate a random color
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
